56b12bf28866581371f0cbf8a084aa2f7bdbb9c1
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-06-29 18:40:24 -0500

    Create chapter9ex2.c

---
+++ chapter9ex2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+char string1 [10];
+char string2 [10];
+
+int main(void)
+{
+    printf("please enter string1\n");
+    fgets(string1, sizeof(string1), stdin);
+    sscanf(string1, "%s", string1);
+
+  printf("please enter string2\n");
+  fgets(string2, sizeof(string2), stdin);
+  sscanf(string2, "%s", string2);
+
+
+if (strcmp(string1,string2) ==0 ){
+
+printf("true");
+}
+
+
+else {
+  printf("false");
+}
+
+
+}
