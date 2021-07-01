2fe799dcc3684c7f5821909d8b1674ed5539ff51
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-07-01 07:26:37 -0500

    Update chapter9ex3.c

--- chapter9ex3.c
+++ chapter9ex3.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
-int count(int number, int *array, int length);
+
+int count(int num, int *array, int lenght);
+
 
 int main()
 {
-	int array[10] = {5, 4, 4, 3, 4, 1, 3, 2, 7, 8};
-	int number;
-// did my best to use the fgets and sscanf but i gain nothing
-  printf("introduce an int number form 1 to 9\n");
-  scanf("%d", &number);
 
-	printf("there is %d times the number %d\n", count(number, array, 10), number);
+
+	int array[10] = {5, 4, 4, 3, 4, 1, 3, 2, 7, 8};
+	char number[10];
+  int numb;
+  printf("please introduce an int number from 1 to 9\n");
+  fgets(number, sizeof(number), stdin);
+  sscanf(number, "%d", &numb);
+
+  
+
+printf("there is %d times the number %s\n", count( numb, array, 10), number);
 
 	return 0;
 }
+
 
 int count(int number, int *array, int length)
 {
