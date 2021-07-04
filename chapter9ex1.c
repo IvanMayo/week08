fa95e316a17e087c5155db97371cf0c725993b15
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-07-04 14:41:32 -0500

    Update chapter9ex1.c

--- chapter9ex1.c
+++ chapter9ex1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
+// : Write a procedure that counts the number of words in a string. (Your 
+//documentation should describe exactly how you define a word.) Write a program to 
+//test your new procedure.
 
 char group[100];
 char s[100];
@@ -16,7 +18,7 @@ int main(void)
     fgets(group, sizeof(group), stdin);
 
     sscanf(group,"%[^\n]s", s);
-
+// counts the number of spaces there are
     
     for (x = 0;   s[x] != '\0';   x++)
     {
