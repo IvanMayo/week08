d46855a0bd205a2b39154efa7c42858fa256dde8
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-07-04 19:50:33 -0500

    Create chapter12.ex2.c

---
+++ chapter12.ex2.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include <string.h>
+
+void sas(int comp){
+
+
+struct tiempo
+{
+int horas;   // my variables
+int minutos;
+int segundos;
+};
+
+// this is basically a previous excerise but with structures, the code speaks for its self
+struct tiempo start, stop, diff;
+
+printf("input the hours, minutes and seconds : ");
+scanf("%d%d%d", &start.horas,&start.minutos, &start.segundos);
+
+// i tried using fgets and sscanf but it desnt work
+
+printf("input the hours, minutes and seconds : ");
+scanf("%d%d%d", &stop.horas,&stop.minutos, &stop.segundos);
+
+if(start.segundos > stop.segundos)
+{
+stop.segundos += 60;
+--stop.minutos;
+}
+
+
+if(start.minutos > stop.minutos)
+{
+stop.minutos += 60;
+--stop.horas;
+}
+
+//here is where the operations are make
+diff.segundos = stop.segundos - start.segundos;
+diff.minutos = stop.minutos - start.minutos;
+diff.horas = stop.horas - start.horas;  
+printf("the difference in time is = %d hours : %d minutes : %d seconds", diff.horas, diff.minutos, diff.segundos);  
+
+}
+
+
+int main()
+{
+
+int comp;// here i call my function 
+sas(comp);
+return 0;
+}
