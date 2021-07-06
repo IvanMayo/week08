4c37b34b70d404d3f1aea87c6c3cdb72a638e1d2
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-07-06 07:54:17 -0500

    Update chapter12.ex2.c

--- chapter12.ex2.c
+++ chapter12.ex2.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 #include <string.h>
+// i tried using fgets and sscanf but it desnt work, gives 0 at the ends and here is the code 
+/*
+struct tiempo start, stop, diff;
+char time[100];
+printf("input the hours, minutes and seconds : ");
+fgets(time, sizeof(time), stdin);
+sscanf(time, "%d%d%d", &start.horas,&start.minutos, &start.segundos);
+
+
+char time2[100];
+printf("input the hours, minutes and seconds : ");
+fgets(time2, sizeof(time2), stdin);
+sscanf(time2, "%d%d%d", &stop.horas,&stop.minutos, &stop.segundos);
+
+*/
 
 void sas(int comp){
 
@@ -41,12 +56,11 @@ diff.minutos = stop.minutos - start.minutos;
 diff.horas = stop.horas - start.horas;  
 printf("the difference in time is = %d hours : %d minutes : %d seconds", diff.horas, diff.minutos, diff.segundos);  
 
-}
+      }
 
 
 int main()
 {
-
 int comp;// here i call my function 
 sas(comp);
 return 0;
