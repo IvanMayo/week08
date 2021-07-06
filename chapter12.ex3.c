068c9d16adbc4140aa563b205204ff2cfd20b4b8
IvanMayo <83649979+IvanMayo@users.noreply.github.com>
2021-07-06 07:46:22 -0500

    Update chapter12.ex3.c

--- chapter12.ex3.c
+++ chapter12.ex3.c
@@ -6,7 +6,7 @@ struct Distance
 
 {
 int feet;
-float inch;
+int inch;
 } 
 
 dist1, dist2, sum;
@@ -14,30 +14,23 @@ dist1, dist2, sum;
 printf("1st distance\n");
   {
   char feet1[100];
-  printf("Enter feet: ");
+  printf("Enter feet and inch 1: ");
   fgets(feet1, sizeof(feet1), stdin);
-  sscanf(feet1, "%d", &dist1.feet);
+  sscanf(feet1, "%d %d", &dist1.feet, &dist1.inch);
 
-
-  char inch1[100];
-  printf("Enter inch: ");
-  fgets(inch1, sizeof(inch1), stdin);
-  sscanf(inch1, "%f", &dist1.inch);
   }
+ 
+  
 
   {
   char feet2[100];
   printf("2nd distance\n");
-  printf("Enter feet: ");
+  printf("Enter feet and inch 2: ");
   fgets(feet2, sizeof(feet2), stdin);
-  scanf("%d", &dist2.feet);
+  sscanf(feet2, "%d %d", &dist2.feet, &dist2.inch);
 // there is a bug where i need to input a number and enter, then the program continues
-
-  char inch2[100];
-  printf("Enter inch: ");
-  fgets(inch2, sizeof(inch2), stdin);
-  scanf("%f", &dist2.inch);
   }
+
 
   // adding feet
   sum.feet = dist1.feet + dist2.feet;
@@ -51,9 +44,11 @@ printf("1st distance\n");
   sum.inch = sum.inch - 12;
   }
 
-  printf("Sum of distances = %d\'-%.1f\"", sum.feet, sum.inch);
+  printf("Sum of distances = %d\'-%.1d\"", sum.feet, sum.inch);
+
 }
 int main()
+
 {
 int medium;
 distances(medium); 
